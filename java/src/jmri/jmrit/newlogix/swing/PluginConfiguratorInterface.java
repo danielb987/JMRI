a14af027478d134d69fe24b2e1d773875b6c1789
Daniel <db123@bergqvist.se>
2018-10-18 22:01:13 +0200

    Update documentation and PluginConfiguratorInterface

--- java/src/jmri/jmrit/newlogix/swing/PluginConfiguratorInterface.java
+++ java/src/jmri/jmrit/newlogix/swing/PluginConfiguratorInterface.java
@@ -1,5 +1,6 @@
 package jmri.jmrit.newlogix.swing;
 
+import java.util.Map;
 import javax.swing.JPanel;
 
 /**
@@ -11,6 +12,7 @@ public interface PluginConfiguratorInterface {
 
     /**
      * Get a configuration panel for this plugin.
+     * This method initializes the panel with an empty configuration.
      * 
      * @param className the name of the class for which to return
      * a configuration panel
@@ -21,12 +23,25 @@ public interface PluginConfiguratorInterface {
     public JPanel getConfigPanel(String className) throws IllegalArgumentException;
     
     /**
+     * Get a configuration panel for this plugin.
+     * 
+     * @param className the name of the class for which to return
+     * a configuration panel
+     * @param config the configuration for this plugin
+     * @return a panel that configures this plugin
+     * @throws IllegalArgumentException if this class does not support the class
+     * with the name given in parameter 'className'
+     */
+    public JPanel getConfigPanel(String className, Map<String, String> config) throws IllegalArgumentException;
+    
+    /**
      * Update the configuration.
      * This method is called then the user has editied the configuration and
      * saves the changes.
      * 
      * @param panel the panel that was returned by getConfigPanel()
+     * @return the configuration for this plugin
      */
-    public void updateConfigFromPanel(JPanel panel);
+    public Map<String, String> getConfigFromPanel(JPanel panel);
 
 }
