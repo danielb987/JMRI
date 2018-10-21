77b81c6c3fca06bc0d52dc703dcbbd0d0742f15f
Daniel <db123@bergqvist.se>
2018-10-21 02:55:54 +0200

    Update java/src/jmri/jmrit/newlogix/NewLogixPreferences.java

--- java/src/jmri/jmrit/newlogix/NewLogixPreferences.java
+++ java/src/jmri/jmrit/newlogix/NewLogixPreferences.java
@@ -33,8 +33,10 @@ public final class NewLogixPreferences extends PreferencesBean {
         this._startNewLogixOnLoad = sharedPreferences.getBoolean(START_NEW_LOGIX_ON_LOAD, this._startNewLogixOnLoad);
         this._allowDebugMode = sharedPreferences.getBoolean(ALLOW_DEBUG_MODE, this._allowDebugMode);
         
-//        String jarFileName = "F:\\Projekt\\Java\\GitHub\\JMRI_NewLogixPlugins\\dist\\JMRI_NewLogixPlugins.jar";
-//        _pluginManager.addJarFile(jarFileName);
+        if (1==0) {
+            String jarFileName = "F:\\Projekt\\Java\\GitHub\\JMRI_NewLogixPlugins\\dist\\JMRI_NewLogixPlugins.jar";
+            _pluginManager.addJarFile(jarFileName);
+        }
         
 /*        
         this.allowRemoteConfig = sharedPreferences.getBoolean(ALLOW_REMOTE_CONFIG, this.allowRemoteConfig);
