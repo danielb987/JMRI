b07dd52b7e3dd4c3cabfd921749f1d17f64f4ec9
Daniel <db123@bergqvist.se>
2019-03-22 23:12:18 +0100

    Add test ActionTurnoutSwingTest

--- java/test/jmri/jmrit/logixng/digital/actions/configureswing/ActionTurnoutSwingTest.java
+++ java/test/jmri/jmrit/logixng/digital/actions/configureswing/ActionTurnoutSwingTest.java
@@ -1,8 +1,11 @@
 package jmri.jmrit.logixng.digital.actions.configureswing;
 
+import java.awt.GraphicsEnvironment;
 import jmri.util.JUnitUtil;
+import jmri.jmrit.logixng.digital.actions.ActionTurnout;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,7 +18,19 @@ public class ActionTurnoutSwingTest {
 
     @Test
     public void testCtor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        
         new ActionTurnoutSwing();
+    }
+    
+    @Test
+    public void testCreatePanel() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        
+        Assert.assertTrue("panel is not null",
+            null != new ActionTurnoutSwing().getConfigPanel());
+        Assert.assertTrue("panel is not null",
+            null != new ActionTurnoutSwing().getConfigPanel(new ActionTurnout("IQ1:DA1")));
     }
     
     // The minimal setup for log4J
---
+++ java/test/jmri/jmrit/logixng/digital/actions/configureswing/PackageTest.java
@@ -0,0 +1,18 @@
+package jmri.jmrit.logixng.digital.actions.configureswing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    ActionTurnoutSwingTest.class,
+})
+
+/**
+ * Invokes complete set of tests in the jmri.jmrit.logixng.actions.configurexml tree
+ *
+ * @author Daniel Bergqvist 2018
+ */
+public class PackageTest {
+
+}
