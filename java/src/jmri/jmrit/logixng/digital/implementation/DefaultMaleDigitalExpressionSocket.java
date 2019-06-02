027f91c7740250d1443aaf862975b6b8e19af39b
Daniel <db123@bergqvist.se>
2019-06-02 21:46:35 +0200

    Add method getFullyFormattedDisplayName(boolean userNameFirst)

--- java/src/jmri/jmrit/logixng/digital/implementation/DefaultMaleDigitalExpressionSocket.java
+++ java/src/jmri/jmrit/logixng/digital/implementation/DefaultMaleDigitalExpressionSocket.java
@@ -143,6 +143,11 @@ public class DefaultMaleDigitalExpressionSocket implements MaleDigitalExpression
     }
 
     @Override
+    public String getFullyFormattedDisplayName(boolean userNameFirst) {
+        return _expression.getFullyFormattedDisplayName(userNameFirst);
+    }
+
+    @Override
     public void addPropertyChangeListener(PropertyChangeListener l, String name, String listenerRef) {
         _expression.addPropertyChangeListener(l, name, listenerRef);
     }
