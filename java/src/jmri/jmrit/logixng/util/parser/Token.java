f55fad85a33c024077ad6d611c295ff9759fa34c
Daniel <db123@bergqvist.se>
2019-06-29 19:24:17 +0200

    Remove unnecessary SuppressFBWarnings

--- java/src/jmri/jmrit/logixng/util/parser/Token.java
+++ java/src/jmri/jmrit/logixng/util/parser/Token.java
@@ -1,19 +1,14 @@
 package jmri.jmrit.logixng.util.parser;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-
 /**
  * A token used by the tokenizer and the parser
  */
 public final class Token {
 
-    @SuppressFBWarnings(value="CI_CONFUSED_INHERITANCE", justification="This field is updated by the Tokenizer class")
-    protected TokenType _tokenType;
-    
-    @SuppressFBWarnings(value="CI_CONFUSED_INHERITANCE", justification="This field is updated by the Tokenizer class")
-    protected String _string;
+    TokenType _tokenType;
+    String _string;
 
-    protected Token() {
+    Token() {
         _tokenType = TokenType.NONE;
         _string = "";
     }
