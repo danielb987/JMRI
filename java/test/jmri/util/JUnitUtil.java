2f66f687697ee4818875b7ce6b6ae2545092feec
Daniel <db123@bergqvist.se>
2026-03-15 22:32:31 +0100

    Dispose TimeProviderManager in JUnitUtil.tearDown()

--- java/test/jmri/util/JUnitUtil.java
+++ java/test/jmri/util/JUnitUtil.java
@@ -163,7 +163,7 @@ public class JUnitUtil {
     static boolean checkSequenceDumpsStack =    Boolean.getBoolean("jmri.util.JUnitUtil.checkSequenceDumpsStack"); // false unless set true
 
     /**
-     * If true, will cause the checkSetUpTearDownSequence check to 
+     * If true, will cause the checkSetUpTearDownSequence check to
      * fail the test in addition to logging.
      * <p>
      * Set from the jmri.util.JUnitUtil.checkSequenceFailsTest environment variable.
@@ -398,6 +398,8 @@ public class JUnitUtil {
      * annotated method.
      */
     public static void tearDown() {
+
+        disposeTimeProviderManager();
 
         // Stop all LogixNG threads
         jmri.jmrit.logixng.util.LogixNG_Thread.stopAllLogixNGThreads();
@@ -801,6 +803,19 @@ public class JUnitUtil {
         InstanceManager.reset(UserPreferencesManager.class);
         // create a test user preferences manager
         InstanceManager.setDefault(UserPreferencesManager.class, new TestUserPreferencesManager());
+    }
+
+
+    public static void initTimeProviderManager() {
+        // Ensure we have the time provider manager
+        InstanceManager.getDefault(jmri.time.TimeProviderManager.class).getCurrentTimeProvider();
+    }
+
+    public static void disposeTimeProviderManager() {
+        var list = InstanceManager.getList(jmri.time.TimeProviderManager.class);
+        if (!list.isEmpty()) {
+            list.get(0).dispose();
+        }
     }
 
     public static void initInternalTurnoutManager() {
